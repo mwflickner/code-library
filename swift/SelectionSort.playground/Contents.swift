18fd860c146cf19f6f2704842315b23b22314914
Matthew <mwflickner@gmail.com>
2017-03-29 22:31:37 -0700

    switched to a class extension

--- swift/SelectionSort.playground/Contents.swift
+++ swift/SelectionSort.playground/Contents.swift
@@ -1,20 +1,22 @@
 import UIKit
 
-func selectionSort<T: Comparable>(array: inout [T]) -> [T] {
-    guard array.count > 1 else {
-        return array
-    }
-    for i in 0..<array.count - 1 {
-        for j in i+1..<array.count {
-            if(array[j] < array[i]){
-                let temp = array[j]
-                array[j] = array[i]
-                array[i] = temp
-            }
-            print(array)
+extension Array where Element:Comparable {
+    mutating func selectionSort() -> Array {
+        guard self.count > 1 else {
+            return self
         }
+        for i in 0..<self.count - 1 {
+            for j in i+1..<self.count {
+                if(self[j] < self[i]){
+                    let temp = self[j]
+                    self[j] = self[i]
+                    self[i] = temp
+                }
+                print(self)
+            }
+        }
+        return self
     }
-    return array
 }
 
 func selectionSortTests(){
@@ -26,11 +28,11 @@ func selectionSortTests(){
     var duplicates = [9,5,1,2,0,5,1]
     let duplicatesSorted = [0,1,1,2,5,5,9]
     
-    assert(selectionSort(array: &empty) == empty)
-    assert(selectionSort(array: &one) == one)
-    assert(selectionSort(array: &test1) == sorted)
-    assert(selectionSort(array: &reverse) == sorted)
-    assert(selectionSort(array: &duplicates) == duplicatesSorted)
+    assert(empty.selectionSort() == [])
+    assert(one.selectionSort() == [1])
+    assert(test1.selectionSort() == sorted)
+    assert(reverse.selectionSort() == sorted)
+    assert(duplicates.selectionSort() == duplicatesSorted)
 }
 
 selectionSortTests()
