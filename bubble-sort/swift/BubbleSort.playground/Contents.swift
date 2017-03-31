e2d865d2d4edffe53edc7e4b8a0236b6a2629e1d
Matthew <mwflickner@gmail.com>
2017-03-30 20:14:10 -0700

    more swifty approach

--- bubble-sort/swift/BubbleSort.playground/Contents.swift
+++ bubble-sort/swift/BubbleSort.playground/Contents.swift
@@ -1,77 +1,42 @@
 import UIKit
 
-func bubbleSort<T: Comparable>(array: inout [T]) -> [T] {
-    guard array.count > 1 else {
-        return array
-    }
-    for i in 0..<array.count {
-        var swapOccured = false
-        for j in 1..<array.count - i {
-            if(array[j] < array[j-1]){
-                let temp = array[j]
-                array[j] = array[j-1]
-                array[j-1] = temp
-                swapOccured = true
+extension Array where Element: Comparable {
+    mutating func bubbleSort() -> Array {
+        guard self.count > 1 else {
+            return self
+        }
+        for i in 0..<self.count {
+            var swapOccured = false
+            for j in 1..<self.count - i {
+                if(self[j] < self[j-1]){
+                    let temp = self[j]
+                    self[j] = self[j-1]
+                    self[j-1] = temp
+                    swapOccured = true
+                }
+            }
+            guard swapOccured else {
+                break
             }
         }
-        guard swapOccured else {
-            break
-        }
-    }
-    return array
-}
-
-func bubbleSort2<T: Comparable>(array: inout [T]) -> [T] {
-    guard array.count > 1 else {
-        return array
-    }
-    while true {
-        var swapOccured = false
-        for j in 0..<array.count - 1 {
-            if(array[j] > array[j+1]){
-                let temp = array[j]
-                array[j] = array[j+1]
-                array[j+1] = temp
-                swapOccured = true
-            }
-        }
-        guard swapOccured else {
-            return array
-        }
+        return self
     }
 }
 
-struct BubbleSortTests {
-    static var empty: [Int] = []
-    static var one = [1]
-    static var test1 = [1,8,2,9,3,7,4,5,6]
-    static var reverse = [9,8,7,6,5,4,3,2,1]
-    static var duplicates = [9,5,1,2,0,5,1]
-    static let sorted = [1,2,3,4,5,6,7,8,9]
-    static let duplicatesSorted = [0,1,1,2,5,5,9]
+func bubbleSortTests() {
+    var empty: [Int] = []
+    var one = [1]
+    var test1 = [1,8,2,9,3,7,4,5,6]
+    var reverse = [9,8,7,6,5,4,3,2,1]
+    var duplicates = [9,5,1,2,0,5,1]
+    let sorted = [1,2,3,4,5,6,7,8,9]
+    let duplicatesSorted = [0,1,1,2,5,5,9]
     
-    private static func bubbleSort1Tests(){
-        assert(bubbleSort(array: &empty) == empty)
-        assert(bubbleSort(array: &one) == one)
-        assert(bubbleSort(array: &test1) == sorted)
-        assert(bubbleSort(array: &reverse) == sorted)
-        assert(bubbleSort(array: &duplicates) == duplicatesSorted)
-    }
-    
-    private static func bubbleSort2Tests(){
-        assert(bubbleSort2(array: &empty) == empty)
-        assert(bubbleSort2(array: &one) == one)
-        assert(bubbleSort2(array: &test1) == sorted)
-        assert(bubbleSort2(array: &reverse) == sorted)
-        assert(bubbleSort2(array: &duplicates) == duplicatesSorted)
-    }
-    
-    static func runTests(){
-        bubbleSort1Tests()
-        bubbleSort2Tests()
-    }
-    
+    assert(empty.bubbleSort() == [])
+    assert(one.bubbleSort() == [1])
+    assert(test1.bubbleSort() == sorted)
+    assert(reverse.bubbleSort() == sorted)
+    assert(duplicates.bubbleSort() == duplicatesSorted)
 }
 
-
-BubbleSortTests.runTests()
+bubbleSortTests()
