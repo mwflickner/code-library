6d39d61273214ced336f56c835e3895dfd14ead2
Matthew <mwflickner@gmail.com>
2018-03-03 11:02:24 -0800

    Cleaned up legacyStack.js

--- stack/javascript/legacyStack.js
+++ stack/javascript/legacyStack.js
@@ -2,23 +2,23 @@ function StackES5 (){
 	this.data = []
 }
 StackES5.prototype.size = function (){
-	return this.data.length
+	return this.data.length;
 }
 StackES5.prototype.push = function (item){
-	this.data.push(item)
+	this.data.push(item);
 }
 StackES5.prototype.pop = function (item){
-	this.data.pop(item)
+	this.data.pop(item);
 }
 StackES5.prototype.peek = function (){
-	return this.data[this.data.length-1]
+	return this.data[this.data.length-1];
 }
 
 var s = new StackES5();
-s.push(1)
-s.push(2)
-s.push(3)
-console.log("3 should be " + s.peek())
-s.pop()
-console.log("2 should be " + s.peek())
-console.log("2 should be " + s.size())
+s.push(1);
+s.push(2);
+s.push(3);
+console.log('3 should be ' + s.peek());
+s.pop();
+console.log('2 should be ' + s.peek());
+console.log('2 should be ' + s.size());
